74264830d5b33ec41dac44ca7faf2d211c705850
Cristian Dominguez <cdominguez@salesforce.com>
2021-11-30 14:57:35 -0300

    chore(deps): bump typedoc to 0.22.6

--- bin/sf-docs.js
+++ bin/sf-docs.js
@@ -6,13 +6,11 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-const { readdirSync, readFileSync, statSync, writeFileSync } = require('fs');
-const { basename, join, resolve } = require('path');
+const { basename, join } = require('path');
 const shell = require('../utils/shelljs');
 const loadRootPath = require('../utils/load-root-path');
 const packageRoot = require('../utils/package-path');
 const { isMultiPackageProject } = require('../utils/project-type');
-const typedoc = require.resolve('typedoc/bin/typedoc');
 
 // eslint-disable-next-line import/order
 let options = require('@salesforce/dev-config/typedoc');
@@ -41,7 +39,7 @@ if (isMultiPackageProject(packageRoot)) {
   outDir = join(packageRoot, outDir, 'tmp');
 }
 
-let command = `${typedoc} --out ${outDir}`;
+let command = `yarn typedoc --out ${outDir}`;
 
 // typedocs does not allow extending configs, so merge the
 // defaults and overrides and put them on the command
@@ -63,34 +61,4 @@ shell.exec(command, {
 if (!isMultiPackageProject(packageRoot)) {
   shell.mv(`${outDir}/*`, `${outDir}/..`);
   shell.rm('-rf', `${outDir}`);
-}
-
-// Fix any leaked local paths in the generated docs
-// See https://github.com/TypeStrong/typedoc/issues/800.
-
-const cleanDocFiles = (dirPath) => {
-  for (const file of readdirSync(dirPath)) {
-    const filePath = join(dirPath, file);
-    const stat = statSync(filePath);
-
-    if (stat.isDirectory()) {
-      cleanDocFiles(filePath);
-    } else {
-      const regex = /Defined in .*[/\\]node_modules[/\\](.*:\d+)/g;
-      let fileContents = readFileSync(filePath).toString();
-      if (fileContents.match(regex)) {
-        fileContents = fileContents.replace(regex, (match, group1) => {
-          group1 = group1.replace(/\\/g, '/');
-          return `Defined in ${group1}`;
-        });
-        writeFileSync(filePath, fileContents);
-      }
-    }
-  }
-};
-
-if (isMultiPackageProject(packageRoot)) {
-  cleanDocFiles(outDir);
-} else {
-  cleanDocFiles(resolve(`${outDir}/..`));
 }
